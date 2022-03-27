49c915df42d87a344de723e85a8c0d7cd6920c82
ZackaryPako <pavlenko_zk_hgs@mail.ru>
2022-03-27 21:53:37 +0800

    Classes for Task5

---
+++ JavaMarathon2021/src/main/java/day5/Car.java
@@ -0,0 +1,29 @@
+package day5;
+
+public class Car {
+
+	private String model;
+	private String color;
+	private int year;
+	
+	public void setModel(String setmodel) {
+		model = setmodel;
+	}
+	public void setColor(String setcolor) {
+		color = setcolor;
+	}
+	public void setYear(int setyear) {
+		year = setyear;
+	}
+	
+	public String getModel() {
+		return model;
+	}
+	public String getColor() {
+		return color;
+	}
+	public int getYearl() {
+		return year;
+	}
+	
+}
---
+++ JavaMarathon2021/src/main/java/day5/Motorbike.java
@@ -0,0 +1,26 @@
+package day5;
+
+public class Motorbike {
+
+	private String model;
+	private String color;
+	private int year;
+	
+	Motorbike(String model, String color, int year){
+		this.model = model;
+		this.color = color;
+		this.year = year;
+	}
+	
+	public String getModel() {
+		return this.model;
+	}
+	public String getColor() {
+		return this.color;
+	}
+	public int getYearl() {
+		return this.year;
+	}
+	
+	
+}
